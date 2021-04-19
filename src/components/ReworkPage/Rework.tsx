2ed1ed496e74a2a780841f0696d00bb5e55e17ce
MartinStrekelj <martin.strekelj123@gmail.com>
2021-04-19 20:51:24 +0200

    fix build

--- src/components/ReworkPage/Rework.tsx
+++ src/components/ReworkPage/Rework.tsx
@@ -1,37 +1,23 @@
-import {
-  Button,
-  Container,
-  Hero,
-  HeroBody,
-  Notification,
-  Subtitle,
-  Title,
-} from "bloomer";
+import { Container, Hero, HeroBody, Subtitle, Title } from "bloomer";
 import React from "react";
 import CR_img from "./cr_img.svg";
 import "./Rework.css";
 
 interface Props {
-    CTA: () => void;
+  CTA: () => void;
 }
 
-const Rework = ({CTA}: Props) => {
+const Rework = ({ CTA }: Props) => {
   return (
     <Hero isSize="large">
       <HeroBody>
         <Container className="containerZ" hasTextAlign="centered">
-          <img className="rework__image" src={CR_img}></img>
-          <Title isSize={1}>
-            {" "}
-            Hey, this page was last updated in Avgust 2020
-          </Title>
+          <img className="rework__image" src={CR_img} alt="rework"></img>
+          <Title isSize={1}>This page was last updated in Avgust 2020</Title>
           <Title>and is currenty under rework.</Title>
           <Subtitle>
-            If you wish to see old legacy page{" "}
-            <span
-              onClick={() => CTA()}
-              className="rework__cta"
-            >
+            If you wish to see old legacy page{" "} 
+            <span onClick={() => CTA()} className="rework__cta">
               click here!
             </span>
           </Subtitle>
