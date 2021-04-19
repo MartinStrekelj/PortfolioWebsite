e49dbd5193125dd9a70e292abcc0d397dadb4d12
MartinStrekelj <martin.strekelj123@gmail.com>
2021-04-19 19:46:25 +0200

    Under rework notification

---
+++ src/components/ReworkPage/Rework.tsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Container,
+  Hero,
+  HeroBody,
+  Notification,
+  Subtitle,
+  Title,
+} from "bloomer";
+import React from "react";
+import CR_img from "./cr_img.svg";
+import "./Rework.css";
+
+interface Props {
+    CTA: () => void;
+}
+
+const Rework = ({CTA}: Props) => {
+  return (
+    <Hero isSize="large">
+      <HeroBody>
+        <Container className="containerZ" hasTextAlign="centered">
+          <img className="rework__image" src={CR_img}></img>
+          <Title isSize={1}>
+            {" "}
+            Hey, this page was last updated in Avgust 2020
+          </Title>
+          <Title>and is currenty under rework.</Title>
+          <Subtitle>
+            If you wish to see old legacy page{" "}
+            <span
+              onClick={() => CTA()}
+              className="rework__cta"
+            >
+              click here!
+            </span>
+          </Subtitle>
+        </Container>
+      </HeroBody>
+    </Hero>
+  );
+};
+
+export default Rework;
